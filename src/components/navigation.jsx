23c647bab983115cd35a66538e9772beeb633cd9
laylar <layla.rowen@gmail.com>
2021-03-22 11:37:57 -0700

    updated custom header and nav text

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-            Jamie Rowen
+            The Joy of Quilting Art
           </a>{' '}
         </div>
 
