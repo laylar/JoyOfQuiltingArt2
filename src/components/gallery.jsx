6a2b13c0431391dc06cd2f24dd6fd936a9b635f1
laylar <layla.rowen@gmail.com>
2021-03-22 17:21:35 -0700

    updated Gallery paragraph

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -5,8 +5,7 @@ export const Gallery = (props) => {
         <div className='section-title'>
           <h2>Gallery</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Here are a few of the amazing things I've made.
           </p>
         </div>
         <div className='row'>
