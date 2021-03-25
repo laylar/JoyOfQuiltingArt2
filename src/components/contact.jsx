545e6fdb404ea62c91737678527424bb098ffe0c
laylar <layla.rowen@gmail.com>
2021-03-25 15:02:22 -0700

    updated contact form paragraph

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -41,7 +41,7 @@ export const Contact = (props) => {
               <div className='section-title'>
                 <h2>Get In Touch</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
+                  Please fill out the form below to send me an email and I will
                   get back to you as soon as possible.
                 </p>
               </div>
